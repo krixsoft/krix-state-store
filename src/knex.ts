7cb6716226ed93aabacdd750dc278478873706e8
Andrey <geass1000@gmail.com>
2019-11-04 00:06:46 +0300

    feat(package): add `init` logic to `Knex` store

--- src/knex.ts
+++ src/knex.ts
@@ -1,13 +1,21 @@
+import * as Rx from 'rxjs';
+import * as _ from 'lodash';
 import { Interfaces } from './shared';
 
 export class Knex<T> {
-  private store: T;
+  private store: any;
+  private sjStoreChanges: Rx.BehaviorSubject<any>;
 
   static create <StoreType> (
     options?: Interfaces.StoreOptions<StoreType>,
   ): Knex<StoreType> {
     const inst = new Knex<StoreType>();
+    inst.init();
     return inst;
+  }
+
+  private init () {
+    this.sjStoreChanges = new Rx.BehaviorSubject(null);
   }
 
   select <StateType = any> (state: string[]): StateType {
