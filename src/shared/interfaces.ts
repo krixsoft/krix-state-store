26c6c51fb373870c34cc3b3ecbca017118dcff2b
Andrey <geass1000@gmail.com>
2019-11-08 01:22:57 +0300

    feat(shared): add `State Change` interface

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -7,3 +7,10 @@ export interface StateAction {
 export interface KnexOptions <StoreType> {
   initStore: StoreType;
 }
+
+export interface StoreChange <StateType = any> {
+  statePath: string;
+  state: string[];
+  oldValue: StateType;
+  newValue: StateType;
+}
