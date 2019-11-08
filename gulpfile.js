da5551b8a29adeac42d0d6fdc06ead69a5de74d0
Andrey <geass1000@gmail.com>
2019-11-08 03:46:52 +0300

    chore(package): fix eslint errors in `gulpfile` file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const gulp = require(`gulp`);
 const ts = require(`gulp-typescript`);
-const tslint = require("gulp-tslint");
-const mocha = require('gulp-mocha');
-const del = require("del");
+const eslint = require(`gulp-eslint`);
+const mocha = require(`gulp-mocha`);
+const del = require(`del`);
 
 const sourceFolder = `./src`;
 const startFile = `index.js`;
 const distFolder = `./dist`;
 
-gulp.task(`clean:dist`, function () {
-  return del(`${distFolder}/*`, { force: true });
+gulp.task(`clean:dist`, async function () {
+  const delResult = await del(`${distFolder}/*`, { force: true });
+  return delResult;
 });
 
 /*
@@ -19,51 +20,44 @@ gulp.task(`clean:dist`, function () {
 const spawn = require(`child_process`).spawn;
 let node;
 
-gulp.task(`start:prod`, function(done) {
-  node = spawn(`node`, [`${distFolder}/${startFile}`], {stdio: `inherit`});
-	done();
+gulp.task(`start:prod`, function (done) {
+  node = spawn(`node`, [`${distFolder}/${startFile}`], { stdio: `inherit` });
+  done();
 });
 
-gulp.task(`start:dev`, function(done) {
-  if (node) node.kill();
-  node = spawn(`node`, [`${distFolder}/${startFile}`], {stdio: `inherit`});
+gulp.task(`start:dev`, function (done) {
+  // eslint-disable-next-line
+  if (node) {
+    node.kill();
+  }
+  node = spawn(`node`, [`${distFolder}/${startFile}`], { stdio: `inherit` });
   node.on(`close`, function (code) {
     if (code === 8) {
       gulp.log(`Error detected, waiting for changes...`);
     }
   });
-	done();
+  done();
 });
-process.on(`exit`, function() {
-  if (node) node.kill();
+process.on(`exit`, function () {
+  // eslint-disable-next-line
+  if (node) {
+    node.kill();
+  }
 });
 
 /**
- * TS Lint
+ * ES Lint
  */
 
-const lintConfig = {
-  configuration: "./tslint.json",
-  formatter: "verbose",
-};
-const lintReportConfig = {
-  emitError: false,
-  summarizeFailureOutput: true,
-  reportLimit: 10,
-};
-
-gulp.task(`tslint:prod`, function () {
+gulp.task(`eslint:prod`, function () {
   return gulp.src(`${sourceFolder}/**/*.ts`)
-    .pipe(tslint(lintConfig))
-    .pipe(tslint.report({
-      ...lintReportConfig,
-      emitError: true,
-    }));
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
-gulp.task(`tslint:dev`, function () {
+gulp.task(`eslint:dev`, function () {
   return gulp.src(`${sourceFolder}/**/*.ts`)
-    .pipe(tslint(lintConfig))
-    .pipe(tslint.report(lintReportConfig));
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
 /**
@@ -73,7 +67,11 @@ gulp.task(`tslint:dev`, function () {
 const tsProject = ts.createProject(`./tsconfig.json`);
 
 gulp.task(`build:move-jts`, gulp.series(function () {
-  return gulp.src([ `${sourceFolder}/**/*.js`, `${sourceFolder}/**/*.d.ts`, `${sourceFolder}/**/*.json` ])
+  return gulp.src([
+    `${sourceFolder}/**/*.js`,
+    `${sourceFolder}/**/*.d.ts`,
+    `${sourceFolder}/**/*.json`,
+  ])
     .pipe(gulp.dest(`${distFolder}`));
 }));
 
@@ -83,16 +81,21 @@ gulp.task(`build`, gulp.series(function () {
     .pipe(gulp.dest(`${distFolder}`));
 }, `build:move-jts`));
 
-gulp.task(`build:prod`, gulp.series(`tslint:prod`, `clean:dist`, `build`));
-gulp.task(`build:dev`, gulp.series(`tslint:dev`, `build`));
+gulp.task(`build:prod`, gulp.series(`eslint:prod`, `clean:dist`, `build`));
+gulp.task(`build:dev`, gulp.series(`eslint:dev`, `build`));
 
-gulp.task(`watch`, gulp.series(`clean:dist`, `build:dev`, `start:dev`, function () {
-  return gulp.watch([
-    `${sourceFolder}/**/*.ts`,
-    `${sourceFolder}/**/*.js`,
-    `${sourceFolder}/**/*.json`,
-  ], gulp.series(`build:dev`, `start:dev`));
-}));
+gulp.task(`watch`, gulp.series(
+  `clean:dist`,
+  `build:dev`,
+  `start:dev`,
+  function () {
+    return gulp.watch([
+      `${sourceFolder}/**/*.ts`,
+      `${sourceFolder}/**/*.js`,
+      `${sourceFolder}/**/*.json`,
+    ], gulp.series(`build:dev`, `start:dev`));
+  },
+));
 
 /**
  * Tests
@@ -102,7 +105,7 @@ gulp.task(`test`, gulp.series(function () {
   return gulp.src(`${distFolder}/**/*.spec.js`)
     .pipe(mocha({ reporter: 'spec', exit: true }))
     .once('error', (error) => {
-        console.error(error);
+      console.error(error);
     });
 }));
 
