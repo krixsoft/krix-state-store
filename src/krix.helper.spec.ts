1b989102103c1be2107538600f87762d29d50476
Andrey <geass1000@gmail.com>
2019-11-09 19:00:09 +0300

    test(package): add tests for `get` logic of `Krix` helper

--- src/krix.helper.spec.ts
+++ src/krix.helper.spec.ts
@@ -58,4 +58,143 @@ describe(`KrixHelper`, () => {
     });
   });
 
+  describe(`get`, () => {
+    const defValue = 256;
+    describe(`when method is invoked with null instead of object`, () => {
+      it('should return undefined or default value', () => {
+        const data: null = null;
+        const dataFromObj = KrixHelper.get(data, '');
+        expect(dataFromObj).to.be.undefined;
+        const dataFromObjWithDef = KrixHelper.get(data, '', defValue);
+        expect(dataFromObjWithDef).to.equal(defValue);
+      });
+    });
+    describe(`when method is invoked with undefined instead of object`, () => {
+      it('should return undefined or default value', () => {
+        const data: undefined = undefined;
+        const dataFromObj = KrixHelper.get(data, '');
+        expect(dataFromObj).to.be.undefined;
+        const dataFromObjWithDef = KrixHelper.get(data, '', defValue);
+        expect(dataFromObjWithDef).to.equal(defValue);
+      });
+    });
+    describe(`when method is invoked with string instead of object`, () => {
+      it('should return undefined or default value', () => {
+        const data: string = 'hello';
+        const dataFromObj = KrixHelper.get(data, '');
+        expect(dataFromObj).to.be.undefined;
+        const dataFromObjWithDef = KrixHelper.get(data, '', defValue);
+        expect(dataFromObjWithDef).to.equal(defValue);
+      });
+    });
+    describe(`when method is invoked with number instead of object`, () => {
+      it('should return undefined or default value', () => {
+        const data: number = 4;
+        const dataFromObj = KrixHelper.get(data, '');
+        expect(dataFromObj).to.be.undefined;
+        const dataFromObjWithDef = KrixHelper.get(data, '', defValue);
+        expect(dataFromObjWithDef).to.equal(defValue);
+      });
+    });
+
+    describe(`when method is invoked with object`, () => {
+      describe(`and path is not string`, () => {
+        it('should return undefined or default value', () => {
+          const data = {
+            hello: `World`,
+          };
+          const dataFromObjWithNumberPath = KrixHelper.get(data, <any>4);
+          expect(dataFromObjWithNumberPath).to.be.undefined;
+          const dataFromObjWithNumberPathWithDef = KrixHelper.get(data, <any>4, defValue);
+          expect(dataFromObjWithNumberPathWithDef).to.equal(defValue);
+          const dataFromObjWithObjectPath = KrixHelper.get(data, <any>{});
+          expect(dataFromObjWithObjectPath).to.be.undefined;
+          const dataFromObjWithObjectPathWithDef = KrixHelper.get(data, <any>{}, defValue);
+          expect(dataFromObjWithObjectPathWithDef).to.equal(defValue);
+          const dataFromObjWithUndefinedPath = KrixHelper.get(data, <any>undefined);
+          expect(dataFromObjWithUndefinedPath).to.be.undefined;
+          const dataFromObjWithUndefinedPathWithDef = KrixHelper.get(data, <any>undefined, defValue);
+          expect(dataFromObjWithUndefinedPathWithDef).to.equal(defValue);
+          const dataFromObjWithNullPath = KrixHelper.get(data, <any>null);
+          expect(dataFromObjWithNullPath).to.be.undefined;
+          const dataFromObjWithNullPathWithDef = KrixHelper.get(data, <any>null, defValue);
+          expect(dataFromObjWithNullPathWithDef).to.equal(defValue);
+        });
+      });
+      describe(`and path is empty string`, () => {
+        it('should return undefined or default value', () => {
+          const data = {
+            hello: `World`,
+          };
+          const dataFromObj = KrixHelper.get(data, '');
+          expect(dataFromObj).to.be.undefined;
+          const dataFromObjWithDef = KrixHelper.get(data, '', defValue);
+          expect(dataFromObjWithDef).to.equal(defValue);
+        });
+      });
+      describe(`and path is a first level property`, () => {
+        describe(`but property doesn't exist`, () => {
+          it('should return undefined or default value', () => {
+            const data = {
+              hello: `World`,
+            };
+            const dataFromObj = KrixHelper.get(data, 'world');
+            expect(dataFromObj).to.be.undefined;
+            const dataFromObjWithDef = KrixHelper.get(data, 'world', defValue);
+            expect(dataFromObjWithDef).to.equal(defValue);
+          });
+        });
+        describe(`and property exists`, () => {
+          it('should return value of property', () => {
+            const data = {
+              hello: `World`,
+            };
+            const dataFromObj = KrixHelper.get(data, 'hello');
+            expect(dataFromObj).to.equal(data.hello);
+          });
+        });
+      });
+      describe(`and path is a second level property`, () => {
+        describe(`and property exists`, () => {
+          it('should return value of property', () => {
+            const data = {
+              hello: `World`,
+              nestObj: { hello: `World 2` },
+            };
+            const dataFromObj = KrixHelper.get(data, 'nestObj.hello');
+            expect(dataFromObj).to.equal(data.nestObj.hello);
+          });
+        });
+      });
+      describe(`and path is a third level property`, () => {
+        describe(`and property exists`, () => {
+          it('should return value of property', () => {
+            const data = {
+              hello: `World`,
+              nestObj: {
+                nestObj: { hello: `World 3` },
+              },
+            };
+            const dataFromObj = KrixHelper.get(data, 'nestObj.nestObj.hello');
+            expect(dataFromObj).to.equal(data.nestObj.nestObj.hello);
+          });
+        });
+        describe(`but property by second part of path doesn't exist`, () => {
+          it('should return or default value', () => {
+            const data = {
+              hello: `World`,
+              nestObj: {
+                nestObj: { hello: `World 3` },
+              },
+            };
+            const dataFromObj = KrixHelper.get(data, 'nestObj.hello.hello');
+            expect(dataFromObj).to.be.undefined;
+            const dataFromObjWithDef = KrixHelper.get(data, 'nestObj.hello.hello', defValue);
+            expect(dataFromObjWithDef).to.equal(defValue);
+          });
+        });
+      });
+    });
+  });
+
 });
