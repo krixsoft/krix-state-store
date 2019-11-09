802ee93900e599e63a1132c91e9faac67f1c052f
Andrey <geass1000@gmail.com>
2019-11-09 17:25:26 +0300

    feat(package): add `package` gulp file instead of `common` gulp file

---
+++ package.gulp.js
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+const gulp = require(`gulp`);
+const ts = require(`gulp-typescript`);
+const eslint = require(`gulp-eslint`);
+const mocha = require(`gulp-mocha`);
+const del = require(`del`);
+
+const sourceFolder = `./src`;
+const distFolder = `./dist`;
+
+exports[`clean:dist`] = async function cleanDist () {
+  const delResult = await del(`${distFolder}/*`, { force: true });
+  return delResult;
+};
+
+/**
+ * ES Lint
+ */
+
+exports[`eslint`] = function eslintProd () {
+  return gulp.src(`${sourceFolder}/**/*.ts`)
+    .pipe(eslint())
+    .pipe(eslint.format());
+};
+
+/**
+ * TS Compilator
+ */
+
+const prodTSConfig = ts.createProject(`./tsconfig.prod.json`);
+const devTSConfig = ts.createProject(`./tsconfig.json`);
+
+exports[`move:jts`] = function moveJTS () {
+  return gulp.src([
+    `${sourceFolder}/**/*.js`,
+    `${sourceFolder}/**/*.d.ts`,
+    `${sourceFolder}/**/*.json`,
+  ])
+    .pipe(gulp.dest(`${distFolder}`));
+};
+
+exports[`build:ts:prod`] = function buildTSProd () {
+  return gulp.src(`${sourceFolder}/**/*.ts`)
+    .pipe(prodTSConfig())
+    .pipe(gulp.dest(`${distFolder}`));
+};
+exports[`build:ts:dev`] = function buildTSDev () {
+  return gulp.src(`${sourceFolder}/**/*.ts`)
+    .pipe(devTSConfig())
+    .pipe(gulp.dest(`${distFolder}`));
+};
+
+exports[`build:src:prod`] = gulp.series(
+  exports[`build:ts:prod`],
+  exports[`move:jts`],
+);
+exports[`build:src:dev`] = gulp.series(
+  exports[`build:ts:dev`],
+  exports[`move:jts`],
+);
+
+exports[`build:prod`] = gulp.series(
+  exports[`eslint`],
+  exports[`clean:dist`],
+  exports[`build:src:prod`],
+);
+exports[`build:dev`] = gulp.series(
+  exports[`eslint`],
+  exports[`build:src:dev`],
+);
+
+exports[`build:watch`] = gulp.series(
+  exports[`clean:dist`] ,
+  exports[`build:dev`],
+  function buildWatch () {
+    return gulp.watch([
+      `${sourceFolder}/**/*.ts`,
+      `${sourceFolder}/**/*.js`,
+      `${sourceFolder}/**/*.json`,
+    ], gulp.series(exports[`build:dev`]));
+  },
+);
+
+/**
+ * Tests
+ */
+
+exports[`test:start`] = function test () {
+  return gulp.src(`${distFolder}/**/*.spec.js`)
+    .pipe(mocha({ reporter: 'spec', exit: true }))
+    .once('error', (error) => {
+      console.error(error);
+    });
+};
+
+exports[`test:watch`] = gulp.series(
+  exports[`test:start`],
+  function testWatch () {
+    return gulp.watch([
+      `${distFolder}/**/*.js`,
+    ], gulp.series(exports[`test:start`]));
+  },
+);
