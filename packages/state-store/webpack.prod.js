c0824bcae8746181912e4b00b1f88f5c2fd7677d
Andrey <geass1000@gmail.com>
2020-04-23 20:50:47 +0300

    feat(pkg/ss): use `commonjs` module for production version of package

--- packages/state-store/webpack.prod.js
+++ packages/state-store/webpack.prod.js
@@ -6,6 +6,7 @@ const path = require('path');
 module.exports = {
   entry: `./src/index.ts`,
   output: {
+    libraryTarget: 'commonjs2',
     path: path.resolve(__dirname, `./dist`),
     filename: `index.js`,
   },
