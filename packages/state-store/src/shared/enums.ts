ed023b08c5dcb14314c62a065e41d0157010a3ba
Artur <artur_huziahmetov@smartexlab.com>
2020-05-01 16:58:02 +0300

    feat(pkg/ss): add `StoreCommandType` enum

---
+++ packages/state-store/src/shared/enums.ts
@@ -0,0 +1,5 @@
+
+export enum StoreCommandType {
+    SetState = 'set-state',
+    AddSubStore = 'add-sub-store',
+}
--- packages/state-store/src/shared/index.ts
+++ packages/state-store/src/shared/index.ts
@@ -1,5 +1,7 @@
 import * as Interfaces from './interfaces';
+import * as Enums from './enums';
 
 export {
   Interfaces,
+  Enums,
 };
